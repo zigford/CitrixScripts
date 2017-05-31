e9822c88420e4303052a5d82966e232942bb3241
Jesse Harris <jpharris@usc.edu.au>
2017-05-31 13:37:00 +1000

    Transcription Logging

--- Scripts/Test-ProductionXenAppMachines.ps1
+++ Scripts/Test-ProductionXenAppMachines.ps1
@@ -1,6 +1,8 @@
+Start-Transcript -Path "$env:Temp\CitrixReport.log"
 Import-Module "$PSScriptPath\USC-Citrix.psm1"
 $ReportEmail = 'team@if83bu.mailclark.ai'
 #Create a HTML Report
 $HTMLReport = Get-CtxProductionMachines | ForEach-Object { Get-CtxXenAppStatus -ComputerName $_ } | ConvertTo-Html
 
 Send-MailMessage -BodyAsHtml -Body ($HTMLReport|Out-String) -From 'wcn-appdev5@usc.internal' -To $ReportEmail -SmtpServer mail.usc.edu.au -Subject 'Citrix Report'
+Stop-Transcript
