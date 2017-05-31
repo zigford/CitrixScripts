25b99c2d9a9021e0135744ca93c7570d1809bc78
Jesse Harris <jpharris@usc.edu.au>
2017-05-31 13:49:29 +1000

    Updated email

--- Scripts/Test-ProductionXenAppMachines.ps1
+++ Scripts/Test-ProductionXenAppMachines.ps1
@@ -1,8 +1,8 @@
 Start-Transcript -Path "$env:Temp\CitrixReport.log"
 $ScriptPath = Split-Path -Path $PSCommandPath -Parent
 Import-Module "$ScriptPath\USC-Citrix.psm1"
-$ReportEmail = 'jpharris@usc.edu.au'
-#$ReportEmail = 'team@if83bu.mailclark.ai'
+#$ReportEmail = 'jpharris@usc.edu.au'
+$ReportEmail = 'team@if83bu.mailclark.ai'
 #Create a HTML Report
 $Report = Get-CtxProductionMachines | ForEach-Object { Get-CtxXenAppStatus -ComputerName $_ } | Out-String
 
