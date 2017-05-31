dfc724cb1c082acb96d2acaf46ea4a69a1beeb4b
Jesse Harris <jpharris@usc.edu.au>
2017-05-31 13:16:33 +1000

    Forced HTML to a string

--- Scripts/Test-ProductionXenAppMachines.ps1
+++ Scripts/Test-ProductionXenAppMachines.ps1
@@ -3,4 +3,4 @@ $ReportEmail = 'jpharris@usc.edu.au'
 #Create a HTML Report
 $HTMLReport = Get-CtxProductionMachines | ForEach-Object { Get-CtxXenAppStatus -ComputerName $_ } | ConvertTo-Html
 
-Send-MailMessage -BodyAsHtml -Body $HTMLReport -From 'wcn-appdev5@usc.internal' -To $ReportEmail -SmtpServer mail.usc.edu.au -Subject 'Citrix Report'
+Send-MailMessage -BodyAsHtml -Body ($HTMLReport|Out-String) -From 'wcn-appdev5@usc.internal' -To $ReportEmail -SmtpServer mail.usc.edu.au -Subject 'Citrix Report'
