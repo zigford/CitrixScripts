c34548c4951cda1fb93269096aacd5bb3ec4fe71
Jesse Harris <jpharris@usc.edu.au>
2017-05-31 12:33:31 +1000

    Moved code into a module. And new script to create and email report

---
+++ Scripts/Test-ProductionXenAppMachines.ps1
@@ -0,0 +1,6 @@
+Import-Module .\USC-Citrix.psm1
+$ReportEmail = 'jpharris@usc.edu.au'
+#Create a HTML Report
+$HTMLReport = Get-CtxProductionMachines | ForEach-Object { Get-CtxXenAppStatus -ComputerName $_ } | ConvertTo-Html
+
+Send-MailMessage -BodyAsHtml -Body $HTMLReport -From 'wcn-appdev5@usc.internal' -To $ReportEmail -SmtpServer mail.usc.edu.au -Subject 'Citrix Report'
---
+++ Scripts/USC-Citrix.psm1
@@ -0,0 +1,31 @@
+﻿function Get-CtxXenAppStatus {
+[CmdletBinding()]
+Param($ComputerName)
+    $OS = gwmi -comp $ComputerName -class win32_operatingsystem
+    [PSCustomObject]@{
+        'ComputerName' = $ComputerName
+        'CacheSpaceFree' = "{0:N0}" -f ((gwmi -ComputerName $ComputerName -Query 'Select FreeSpace From Win32_LogicalDisk Where DeviceID = "D:"').FreeSpace /1gb)
+        'Uptime' = 	$OS.ConvertToDateTime($OS.LastBootUpTime)
+        'Sessions' = (gwmi -comp $ComputerName -class win32_userprofile|?{$_.Loaded -eq 'True' -and $_.LocalPath -notmatch 'Ctx' -and $_.LocalPath -notmatch 'Citrix' -and $_.LocalPath -match 'Users'} | Measure-Object).Count
+    }#| ?{(Get-Date $_.Uptime) -lt (Get-Date).AddDays(-7)}
+}
+
+function Get-CtxProductionMachines {
+    [CmdLetBinding()]
+    Param(
+    )
+
+    Write-Verbose "Student Machines"
+    for ($i=3;$i -le 26;$i++) {
+        "vxa-ctxstu$($i.ToString("00"))" 
+    }
+
+    for ($i=3;$i -le 4;$i++) {
+        "vxa-ctxstf$($i.ToString("00"))"
+    }
+
+    for ($i=3;$i -le 4;$i++) {
+        "vxa-ctxits$($i.ToString("00"))"
+    }
+
+}
